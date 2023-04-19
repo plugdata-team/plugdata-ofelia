66a48bc8977b72def231ed939ca36ec4d3f45b3d
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-19 04:20:18 +0200

    Let build system create full ofelia package

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-mkdir bin
-
-
 if [ "$(uname)" == "Darwin" ]; then
       cd ./ofxOfelia/macOSExternal
       make
-      cp -f ./bin/ofelia.pd_darwin.app/Contents/MacOS/ofelia.pd_darwin ../../bin/ofelia.pd_darwin
+      cp -rf ../ofelia ../../ofelia
+      cp -f ./bin/ofelia.pd_darwin.app/Contents/MacOS/ofelia.pd_darwin ../../ofelia/ofelia.pd_darwin
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       cd ./ofxOfelia/LinuxExternal
       make
-      cp -f ./bin/ofelia.pd_linux ../../bin/ofelia.pd_linux
+      cp -rf ../ofelia ../../ofelia
+      cp -f ./bin/ofelia.pd_linux ../../ofelia/ofelia.pd_linux
 fi
