8718d016ae3ecf68057882776426ee1458f4889a
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-19 13:24:02 +0200

    Trying to fix Linux build

--- install_libs.sh
+++ install_libs.sh
@@ -4,6 +4,6 @@ if [ "$(uname)" == "Darwin" ]; then
       ./openFrameworks/scripts/osx/download_libs.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       sudo ./openFrameworks/scripts/linux/ubuntu/install_dependencies.sh -y
-      ./openFrameworks/scripts/linux/download_libs.sh
+      ./openFrameworks/scripts/linux/download_libs.sh -a 64gcc6
       ./openFrameworks/scripts/linux/compileOF.sh
 fi
