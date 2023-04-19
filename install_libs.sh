91e09f09dff9194ee76e1480f93a37be57a4b357
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-19 03:06:54 +0200

    try compiling on ubuntu again

--- install_libs.sh
+++ install_libs.sh
@@ -3,7 +3,7 @@
 if [ "$(uname)" == "Darwin" ]; then
       ./openFrameworks/scripts/osx/download_libs.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-      ./openFrameworks/scripts/linux/debian/install_dependencies.sh -y
+      sudo ./openFrameworks/scripts/linux/ubuntu/install_dependencies.sh -y
       ./openFrameworks/scripts/linux/download_libs.sh
       ./openFrameworks/scripts/linux/compileOF.sh
 fi
