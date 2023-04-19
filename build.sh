26eb4b9e859779b7bd33fa9a8fdf47c71856ce27
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-19 16:10:13 +0200

    Fixed macOS problem

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ if [ "$(uname)" == "Darwin" ]; then
       cp -rf ../ofelia ../../
       cp -f ./bin/ofelia.pd_darwin.app/Contents/MacOS/ofelia.pd_darwin ../../ofelia/ofelia.pd_darwin
       mkdir ../../ofelia/libs
-      cp -f ./bin/ofelia.pd_darwin.app/Contents/Frameworks/libfmod.dylib ../../ofelia/ofelia.pd_darwin
+      cp -f ./bin/ofelia.pd_darwin.app/Contents/Frameworks/libfmod.dylib ../../ofelia/libs/libfmod.dylib
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       cd ./ofxOfelia/LinuxExternal
       make
