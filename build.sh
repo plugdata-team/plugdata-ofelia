7b18d9edfbaf35b30d1ac7eed9b7c201ccc88b0e
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-20 19:39:26 +0200

    Linux script fix

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       CC=clang CXX=clang++ CFLAGS=-fPIC make
       cp -rf ../ofelia ../../
       cp -f ./bin/ofelia.* ../../ofelia
-      cp ./deps-scripts ../../ofelia
+      cp -rf ./deps-scripts ../../ofelia
       ./bundle_libs.sh ../../ofelia/ofelia.*
       #cp -f ./bin/ofelia.pd_darwin.app/Contents/Frameworks/libfmodex.dylib ../../ofelia/ofelia.pd_darwin
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
