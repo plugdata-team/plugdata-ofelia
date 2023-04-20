6f87809e5d27d3612fc4f6b6303e6318280ae4be
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-20 17:29:35 +0200

    Fixed copying for arm build

--- build.sh
+++ build.sh
@@ -11,7 +11,11 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       cd ./ofxOfelia/LinuxExternal
       CC=clang CXX=clang++ CFLAGS=-fPIC make
       cp -rf ../ofelia ../../
-      cp -f ./bin/ofelia.pd_linux ../../ofelia/ofelia.pd_linux
+      cp -f ./bin/ofelia.* ../../ofelia
       mkdir ../../ofelia/libs
       #cp -f ./bin/ofelia.pd_darwin.app/Contents/Frameworks/libfmodex.dylib ../../ofelia/ofelia.pd_darwin
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+      cd ./ofxOfelia/WindowsExternal
 fi
+
+l_arm
--- install_libs.sh
+++ install_libs.sh
@@ -3,10 +3,10 @@
 if [ "$(uname)" == "Darwin" ]; then
       ./openFrameworks/scripts/osx/download_libs.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-      echo $(gcc -v)
       sudo ./openFrameworks/scripts/linux/ubuntu/install_dependencies.sh -y
       ./openFrameworks/scripts/linux/download_libs.sh
       rm -rf ./openFrameworks/libs/kiss
       ./kiss/kiss.sh
-      #CC=clang CXX=clang++ CFLAGS=-fPIC ./openFrameworks/scripts/linux/compileOF.sh
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+     ./openFrameworks/scripts/vs/download_libs.sh
 fi
