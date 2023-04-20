4479f5b05d97401a57564d18e833189fdcc6e827
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-20 18:30:18 +0200

    Fixed dynamic library lookup

--- build.sh
+++ build.sh
@@ -5,14 +5,13 @@ if [ "$(uname)" == "Darwin" ]; then
       make
       cp -rf ../ofelia ../../
       cp -f ./bin/ofelia.pd_darwin.app/Contents/MacOS/ofelia.pd_darwin ../../ofelia/ofelia.pd_darwin
-      mkdir ../../ofelia/libs
-      cp -f ./bin/ofelia.pd_darwin.app/Contents/Frameworks/libfmod.dylib ../../ofelia/libs/libfmod.dylib
+      cp -f ./bin/ofelia.pd_darwin.app/Contents/Frameworks/libfmod.dylib ../../ofelia/libfmod.dylib
+      install_name_tool -change @executable_path/../Frameworks/libfmod.dylib @rpath/libfmod.dylib ../../ofelia/ofelia.pd_darwin
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
       cd ./ofxOfelia/LinuxExternal
       CC=clang CXX=clang++ CFLAGS=-fPIC make
       cp -rf ../ofelia ../../
       cp -f ./bin/ofelia.* ../../ofelia
-      mkdir ../../ofelia/libs
       cp ./deps-scripts ../../ofelia
       #cp -f ./bin/ofelia.pd_darwin.app/Contents/Frameworks/libfmodex.dylib ../../ofelia/ofelia.pd_darwin
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
--- ofxOfelia/LinuxExternal/config.make
+++ ofxOfelia/LinuxExternal/config.make
@@ -18,7 +18,7 @@ ifeq ($(PLATFORM_OS),Linux)
         APPNAME = ofelia.pd_linux
     endif
     PROJECT_CFLAGS = -fPIC -Wall -Wno-sign-compare -Wno-unused-variable -Wno-maybe-uninitialized -I../libs/ofxPd/libs/libpd/pure-data/src
-    PROJECT_LDFLAGS = -rdynamic -shared -Wl,-rpath,./libs,-Bsymbolic
+    PROJECT_LDFLAGS = -rdynamic -shared
     PROJECT_EXTERNAL_SOURCE_PATHS = ../src
     PROJECT_DEFINES = LUA_USE_LINUX HAVE_LIBDL TARGET_EXTERNAL
     PROJECT_OPTIMIZATION_CFLAGS_RELEASE = -O3 -DNDEBUG
