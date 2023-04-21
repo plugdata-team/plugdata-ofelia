8a1e46a419f3dca0ceb608848c4b519b5bb41313
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-21 16:57:19 +0200

    Added missing source file

---
+++ ofxOfelia/src/ofxOfeliaAsync.cpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "ofxOfeliaAsync.h"
+
+extern void ofelia_set_run_loop(std::function<void()>);
+extern void ofelia_call_async(std::function<void()>);
+extern void ofelia_audio_lock();
+extern void ofelia_audio_unlock();
+
+ofxOfeliaAudioLock::ofxOfeliaAudioLock()
+{
+    ofelia_audio_lock();
+}
+
+ofxOfeliaAudioLock::~ofxOfeliaAudioLock()
+{
+    ofelia_audio_unlock();
+}
+
+
+ofxOfeliaLock::ofxOfeliaLock()
+{
+    ofeliaLock.lock();
+}
+
+ofxOfeliaLock::~ofxOfeliaLock()
+{
+    ofeliaLock.unlock();
+}
+
+std::recursive_mutex ofxOfeliaLock::ofeliaLock;
+
+void ofxOfeliaAsync::setRunLoop(std::function<void()> fn)
+{
+    const ofxOfeliaLock ofxLock;
+    ofelia_set_run_loop(fn);
+}
+
+void ofxOfeliaAsync::callAsync(std::function<void()> fn)
+{
+    ofelia_call_async([fn](){
+        const ofxOfeliaLock ofxLock;
+        fn();
+    });
+}
+
