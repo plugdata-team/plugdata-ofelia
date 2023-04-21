a11520529c0606583e3a15fc7f161c8850a48502
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-21 22:22:11 +0200

    Fix for packaging script for Windows

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@ cd .\ofxOfelia\WindowsExternal
 msbuild ofelia.sln /p:configuration=release /p:PlatformToolset=v142 /p:platform=x64
 DEL ".\bin\pd.dll"
 DEL ".\bin\pthreadVC3.dll"
-COPY  "..\ofelia" "%CD%\..\..\ofelia"
-COPY  ".\bin\*.dll" "%CD%\..\..\ofelia\"
+MOVE  "%CD%\..\ofelia" "%CD%\..\..\ofelia"
+COPY  "\bin\*.dll" "%CD%\..\..\ofelia\"
