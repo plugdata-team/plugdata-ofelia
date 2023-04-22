278da26b8d43bd1fd9872f17d07054a8c8d0c281
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-22 17:35:28 +0200

    Notarise fixes

--- .github/scripts/package-macOS.sh
+++ .github/scripts/package-macOS.sh
@@ -7,6 +7,6 @@
 zip -vr ofelia.zip ./ofelia -x "*.DS_Store"
 
 # Notarise zip
-xcrun notarytool store-credentials "notary_login" --apple-id ${AC_USERNAME} --password ${AC_PASSWORD} --team-id "7SV7JPRR2L"
+xcrun notarytool store-credentials "notary_login" --apple-id "${AC_USERNAME}" --password "${AC_PASSWORD}" --team-id "7SV7JPRR2L"
 xcrun notarytool submit ./ofelia.zip --keychain-profile "notary_login" --wait
 #xcrun stapler staple "ofelia.zip"
