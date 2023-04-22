75e4b969c2705a27cf13dafb28520e0bae15f2d4
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-22 17:03:53 +0200

    Notarisation fix

--- .github/scripts/package-macOS.sh
+++ .github/scripts/package-macOS.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Sign plugins
-/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./ofelia.pd_darwin
-/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./libs/libfmod.dylib
+/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./ofelia/ofelia.pd_darwin
+/usr/bin/codesign --force -s "Developer ID Application: Timothy Schoen (7SV7JPRR2L)" ./ofelia/libs/libfmod.dylib
 
-zip -r ofelia.zip ofelia
+zip -vr ofelia.zip ./ofelia -x "*.DS_Store"
 
 # Notarise zip
-xcrun notarytool store-credentials "notary_login" --apple-id ${AC_USERNAME} --password ${AC_PASSWORD} --team-id "7SV7JPRR2L"
+xcrun notarytool store-credentials "notary_login" --apple-id "timschoen123@gmail.com" --password "tzbo-geux-nncv-brtd" --team-id "7SV7JPRR2L"
 xcrun notarytool submit ./ofelia.zip --keychain-profile "notary_login" --wait
 #xcrun stapler staple "ofelia.zip"
