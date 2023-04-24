3cbe9a3249b867531a1051047a875047238fc4d8
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-24 17:39:52 +0200

    Back to building on ubuntu 20.04

--- .github/scripts/linux-install-libs.sh
+++ .github/scripts/linux-install-libs.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 export TZ="Europe/Amsterdam"
 
 # Install basics first, this can help to prevent package conflicts
-apt install -y build-essential ccache clang cmake curl git gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-vaapi libasound2-dev libassimp-dev libcairo-dev libcurl4-openssl-dev libfreetype6-dev libglu1-mesa-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-0 libjack-jackd2-dev libudev-dev libunwind-dev libx11-dev libxcomposite-dev libxcursor-dev libxext-dev libxinerama-dev libxrandr-dev libxrender-dev lsb-release mesa-common-dev pax-utils pkg-config python3 python3-pip rsync sudo wget patchelf libsndfile1-dev
+apt install -y build-essential ccache clang cmake curl git gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-vaapi libasound2-dev libassimp-dev libcairo-dev libcurl4-openssl-dev libfreetype6-dev libglu1-mesa-dev libgstreamer-plugins-base1.0-dev libgstreamer1.0-0 libjack-jackd2-dev libudev-dev libunwind-dev libx11-dev libxcomposite-dev libxcursor-dev libxext-dev libxinerama-dev libxrandr-dev libxrender-dev lsb-release mesa-common-dev pax-utils pkg-config python3 python3-pip rsync sudo wget patchelf libsndfile1-dev openal
 
 # Then we try to install all of OF's packages ourselves, their script can fail in noninteractive mode
 apt install --ignore-missing adwaita-icon-theme at-spi2-core default-libmysqlclient-dev freeglut3 \
@@ -77,7 +77,7 @@ libmysofa0 libnorm1 libpgm-5.2-0 libpostproc55 librubberband2 libsodium23 \
 libvidstab1.1 libzmq5 \
 
 # Make sure all OF packages are installed
-DEBIAN_FRONTEND=noninteractive ./openFrameworks/scripts/linux/debian/install_dependencies.sh -y
+DEBIAN_FRONTEND=noninteractive ./openFrameworks/scripts/linux/ubuntu/install_dependencies.sh -y
 
 # Download OF libraries
 ./openFrameworks/scripts/linux/download_libs.sh
