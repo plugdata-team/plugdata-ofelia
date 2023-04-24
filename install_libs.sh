b79b688bb9c7beb42f6bbb1dd244cfb530091623
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-24 14:01:36 +0200

    Trying to prevent interactive installers

--- install_libs.sh
+++ install_libs.sh
@@ -3,7 +3,7 @@
 if [ "$(uname)" == "Darwin" ]; then
       ./openFrameworks/scripts/osx/download_libs.sh
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-      sudo ./openFrameworks/scripts/linux/debian/install_dependencies.sh -y
+      DEBIAN_FRONTEND=noninteractive ./openFrameworks/scripts/linux/debian/install_dependencies.sh -y
       ./openFrameworks/scripts/linux/download_libs.sh
       rm -rf ./openFrameworks/libs/kiss
       ./kiss/kiss.sh
