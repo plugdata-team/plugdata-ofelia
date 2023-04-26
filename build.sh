7f8d57f7336947a9bf47b99da56388614d90efd6
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-04-26 15:41:17 +0200

    Apply patch to openFrameworks to fix support for newer gstreamer versions

--- build.sh
+++ build.sh
@@ -15,6 +15,16 @@ if [ "$(uname)" == "Darwin" ]; then
       cp -f ./bin/ofelia.pd_darwin   ../../ofelia/ofelia.pd_darwin
 # Build for Linux
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+
+      # Apply OF git patch for video playback
+      pushd ./openFrameworks
+      if [[-f gstreamer-fix.patch]]; then
+      else
+      wget https://github.com/openFrameworks/openFrameworks/commit/bd4042344dc9670770754374607d2ca8190f9476.patch -o gstreamer-fix.patch
+      git apply gstreamer-fix.patch
+      fi
+      popd
+
       cd ./ofxOfelia/LinuxExternal
       CC=clang CXX=clang++ CFLAGS=-fPIC make
       # Copy output file with either .l_arm and .pd_linux extensions
