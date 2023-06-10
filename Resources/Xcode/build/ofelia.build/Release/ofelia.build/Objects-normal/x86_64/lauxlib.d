81019e6b401445b15ead8ae515846889df459ecd
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-10 20:05:44 +0200

    Updated xcode project

--- Resources/Xcode/build/ofelia.build/Release/ofelia.build/Objects-normal/x86_64/lauxlib.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lauxlib.c \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lprefix.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lua.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/luaconf.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lauxlib.h
--- Resources/Xcode/build/ofelia.build/Release/ofelia.build/Script-FDC3FED41EEBC9FF00FF383D.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# copy libfmod.dylib and change @executable_path to @loader_path
-mkdir -p "$SRCROOT/bin/libs"
-cp -rf "$SRCROOT/../../Libraries/openFrameworks;/libs/fmod/lib/osx/libfmod.dylib" "$SRCROOT/bin/libs"
-install_name_tool -change @executable_path/../Frameworks/libfmod.dylib @loader_path/libs/libfmod.dylib $BUILT_PRODUCTS_DIR/ofelia
-
