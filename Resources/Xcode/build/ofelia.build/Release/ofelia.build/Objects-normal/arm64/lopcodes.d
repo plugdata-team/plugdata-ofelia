8a5b1101d40e42b08790147c37428181d9f0ef7c
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-10 18:38:14 +0200

    Cleaned up

--- Resources/Xcode/build/ofelia.build/Release/ofelia.build/Objects-normal/arm64/lopcodes.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lopcodes.c \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lprefix.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lopcodes.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/llimits.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lua.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/luaconf.h
--- Resources/Xcode/build/ofelia.build/Release/ofelia.build/Objects-normal/arm64/ofxOfeliaSignal.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/timschoen/Projecten/plugdata/Libraries/plugdata-ofelia/ofxOfelia/src/Runner/ofxOfeliaSignal.cpp \
-  /Users/timschoen/Projecten/plugdata/Libraries/plugdata-ofelia/ofxOfelia/src/Runner/ofxOfeliaSignal.h
