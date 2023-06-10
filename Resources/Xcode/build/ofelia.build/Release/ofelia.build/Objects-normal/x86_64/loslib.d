cc020a9b546bd65d8d570138d2e2f3ec1092bce7
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-10 18:38:14 +0200

    Cleaned up

--- Resources/Xcode/build/ofelia.build/Release/ofelia.build/Objects-normal/x86_64/loslib.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/loslib.c \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lprefix.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lua.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/luaconf.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lauxlib.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lualib.h
