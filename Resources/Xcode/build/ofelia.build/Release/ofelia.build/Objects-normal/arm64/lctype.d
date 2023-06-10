35e5bdaa277bb3e7833f9b82b1f2e3adb06c28a0
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-10 20:05:44 +0200

    Updated xcode project

--- Resources/Xcode/build/ofelia.build/Release/ofelia.build/Objects-normal/arm64/lctype.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lctype.c \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lprefix.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lctype.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/lua.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/luaconf.h \
-  /Users/timschoen/Projecten/plugdata-ofelia/Libraries/ofxLua/libs/lua/llimits.h
