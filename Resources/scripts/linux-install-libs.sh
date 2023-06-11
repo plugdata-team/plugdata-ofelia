a7f166192965a83865680daed7630f7ee94bd886
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-11 04:52:59 +0200

    Dependency install script fix

--- Resources/scripts/linux-install-libs.sh
+++ Resources/scripts/linux-install-libs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Function to install packages on Ubuntu and Debian
-install_ubuntu_packages() {
+install_debian_packages() {
     apt-get update
     apt-get install -y gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-vaapi libgstreamer-plugins-base1.0-dev libgstreamer1.0-0 freeglut3-dev libasound2-dev libxmu-dev libxxf86vm-dev g++ libgl1-mesa-dev libglu1-mesa-dev libraw1394-dev libudev-dev libdrm-dev libglew-dev libopenal-dev libsndfile-dev libfreeimage-dev libcairo2-dev libfreetype6-dev libssl-dev libpulse-dev libusb-1.0-0-dev libgtk${GTK_VERSION}-dev libopencv-dev libegl1-mesa-dev libgles1 libgles2-mesa-dev libassimp-dev librtaudio-dev libboost-filesystem-dev libglfw3-dev liburiparser-dev libcurl4-openssl-dev libpugixml-dev libpoco-dev libgconf-2-4
     /Users/timschoen/Projecten/plugdata/Libraries/plugdata-ofelia/Libraries/openFrameworks/scripts/linux/debian/install_dependencies.sh
@@ -66,18 +66,18 @@ install_dependencies() {
     fi
 
     if [ -f /etc/debian_version ]; then
-        install_ubuntu_packages
+        install_debian_packages
         return
     fi
 
-    install_ubuntu_packages
+    install_debian_packages
 }
 
 install_dependencies
 
 # Manually build kissfft, because OF builds it without the fPIC flag
-rm -rf ./openFrameworks/libs/kiss
-./kiss/kiss.sh
+rm -rf ./Libraries/openFrameworks/libs/kiss
+./Libraries/kiss/kiss.sh
 
 # Install copydeps python package for shared library packaging
 pip3 install copydeps
