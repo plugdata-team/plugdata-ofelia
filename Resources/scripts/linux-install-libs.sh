e93c70e288e2cf5934591d039dd31764afaaa8dd
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-11 17:59:43 +0200

    Fixed for install-libs script

--- Resources/scripts/linux-install-libs.sh
+++ Resources/scripts/linux-install-libs.sh
@@ -6,13 +6,13 @@ install_debian_packages() {
     export TZ="Europe/Amsterdam"
     apt-get update
     apt-get install -y gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-ugly gstreamer1.0-vaapi libgstreamer-plugins-base1.0-dev libgstreamer1.0-0 freeglut3-dev libasound2-dev libxmu-dev libxxf86vm-dev g++ libgl1-mesa-dev libglu1-mesa-dev libraw1394-dev libudev-dev libdrm-dev libglew-dev libopenal-dev libsndfile-dev libfreeimage-dev libcairo2-dev libfreetype6-dev libssl-dev libpulse-dev libusb-1.0-0-dev libgtk${GTK_VERSION}-dev libopencv-dev libegl1-mesa-dev libgles1 libgles2-mesa-dev libassimp-dev librtaudio-dev libboost-filesystem-dev libglfw3-dev liburiparser-dev libcurl4-openssl-dev libpugixml-dev libpoco-dev libgconf-2-4
-    ./Libraries/openFrameworks/scripts/linux/debian/install_dependencies.sh
+    ./Libraries/openFrameworks/scripts/linux/debian/install_dependencies.sh -y
 }
 
 # Function to install packages on Fedora
 install_fedora_packages() {
     dnf install -y gstreamer1-plugins-base gstreamer1-plugins-good gstreamer1-plugins-ugly gstreamer1-vaapi alsa-lib-devel libassimp-devel cairo-devel libcurl-devel freetype-devel mesa-libGLU-devel gstreamer1-devel gstreamer1 jack-audio-connection-kit-devel systemd-devel libunwind-devel libX11-devel libXcomposite-devel libXcursor-devel libXext-devel libXinerama-devel libXrandr-devel libXrender-devel redhat-lsb mesa-libGL-devel pax-utils pkg-config python3 python3-pip rsync wget patchelf libsndfile-devel libXi-devel
-    ./Libraries/openFrameworks/scripts/linux/fedora/install_dependencies.sh
+    ./Libraries/openFrameworks/scripts/linux/fedora/install_dependencies.sh -y
 }
 
 # Function to install packages on openSUSE
@@ -26,7 +26,7 @@ install_opensuse_packages() {
 
 # Function to install packages on openSUSE
 install_arch_packages() {
-   ./Libraries/openFrameworks/scripts/linux/archlinux/install_dependencies.sh
+   ./Libraries/openFrameworks/scripts/linux/archlinux/install_dependencies.sh -y
 }
 
 
