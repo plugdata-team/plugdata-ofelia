674c4b6409365e7159471191d8ee3537b6c4911a
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-11 03:59:50 +0200

    Improved packaging script

--- Resources/scripts/create-binary-data.py
+++ Resources/scripts/create-binary-data.py
@@ -32,9 +32,34 @@ for i in range(num_chunks):
     
     with open(cpp_file_path, 'w') as cpp_file:
         cpp_file.write('#include <vector>\n\n')
-        cpp_file.write('std::vector<unsigned char> zip_data = {\n')
+        cpp_file.write(f'std::vector<unsigned char> ofelia_binary_data_{i} = ')
+        cpp_file.write('{\n')
         
         for byte in chunk:
             cpp_file.write(str(byte) + ', ')
         
         cpp_file.write('\n};\n')
+
+
+header_file_path = os.path.join(output_dir, 'ofelia_binary_data.h')
+
+with open(header_file_path, 'w') as header_file:
+    header_file.write('#ifndef OFELIA_BINARY_DATA_H\n')
+    header_file.write('#define OFELIA_BINARY_DATA_H\n\n')
+
+    for i in range(num_chunks):
+        header_file.write(f'extern std::vector<unsigned char> ofelia_binary_data_{i};\n')
+
+    header_file.write('\n')
+
+    header_file.write('inline std::vector<unsigned char> getOfeliaBinaryData()\n')
+    header_file.write('{\n')
+    header_file.write('    std::vector<unsigned char> appended_data;\n\n')
+
+    for i in range(num_chunks):
+        header_file.write(f'    appended_data.insert(appended_data.end(), ofelia_binary_data_{i}.begin(), ofelia_binary_data_{i}.end());\n')
+
+    header_file.write('\n    return appended_data;\n')
+    header_file.write('}\n\n')
+
+    header_file.write('#endif // OFELIA_BINARY_DATA_H\n')
