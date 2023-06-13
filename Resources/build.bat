21f6ecacc2e733efd7cd51e1d2367a8ad43dcf89
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-13 20:53:10 +0200

    Windows build fix

--- Resources/build.bat
+++ Resources/build.bat
@@ -8,4 +8,4 @@ cd ..\..
 cd .\Resources\VisualStudio
 msbuild ofelia.sln /p:configuration=release /p:PlatformToolset=v142 /p:platform=x64
 MOVE  "%CD%\..\ofelia" "%CD%\..\..\ofelia"
-COPY  "bin\*.dll" "%CD%\..\..\ofelia"
+COPY  "bin\*" "%CD%\..\..\ofelia"
