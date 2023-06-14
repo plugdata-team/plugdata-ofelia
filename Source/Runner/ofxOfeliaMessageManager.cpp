501acadb18d5c08c51278c095cb8f53979e322ca
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-15 00:36:49 +0200

    Cleaning up

--- Source/Runner/ofxOfeliaMessageManager.cpp
+++ Source/Runner/ofxOfeliaMessageManager.cpp
@@ -6,4 +6,79 @@
 
 #include "ofxOfeliaMessageManager.h"
 
+
+ofxOfeliaMessageManager::ofxOfeliaMessageManager(int portNumber)
+{
+    pipe.bind(portNumber, portNumber + 1); // TODO: terminate if this fails
+    returnPipe.bind(portNumber + 2, portNumber + 3);
+    
+    // Let plugdata know we have started up
+    returnPipe.sendMessage(ofx_lua_init);
+    
+    shouldQuit = false;
+    
+    udpThread = std::thread(&ofxOfeliaMessageManager::run, this);
+    
+    instance = this;
+}
+
+ofxOfeliaMessageManager::~ofxOfeliaMessageManager()
+{
+    shouldQuit = true;
+    udpThread.join();
+}
+
+
+void ofxOfeliaMessageManager::run()
+{
+    std::string message;
+    while (!shouldQuit)
+    {
+        message = pipe.receive();
+        
+        if(message.empty())
+        {
+            std::this_thread::sleep_for(std::chrono::microseconds(500));
+            continue;
+        }
+            
+        std::string args;
+        
+        auto istream = std::stringstream(message);
+
+        // Remove type of message to return later
+        ofxMessageType messageType;
+        istream.read(reinterpret_cast<char *>(&messageType), sizeof(ofxMessageType));
+
+        /*
+        if (messageType == ofx_audio_block)
+        {
+            signalQueue.enqueue();
+            continue;
+        } */
+        
+        // Put args into string
+        char ch;
+        while (istream.get(ch)) {
+            args += ch;  // Append each character to the string
+        }
+        
+        messageQueue.enqueue({messageType, args});
+    }
+}
+
+bool ofxOfeliaMessageManager::receiveMessage(ofxMessageType& type, std::string& message)
+{
+    std::pair<ofxMessageType, std::string> result;
+    auto dequeued = messageQueue.try_dequeue(result);
+    if(dequeued)
+    {
+        type = result.first;
+        message = result.second;
+        return true;
+    }
+    
+    return false;
+}
 ofxOfeliaMessageManager* ofxOfeliaMessageManager::instance = nullptr;
+
--- Source/Runner/ofxOfeliaMessageManager.h
+++ Source/Runner/ofxOfeliaMessageManager.h
@@ -16,40 +16,11 @@
 
 struct ofxOfeliaMessageManager {
     
-    ofxOfeliaMessageManager(int portNumber)
-    {
-        pipe.bind(portNumber, portNumber + 1); // TODO: terminate if this fails
-        returnPipe.bind(portNumber + 2, portNumber + 3);
-        
-        // Let plugdata know we have started up
-        returnPipe.sendMessage(ofx_lua_init);
-        
-        shouldQuit = false;
-        
-        udpThread = std::thread(&ofxOfeliaMessageManager::run, this);
-        
-        instance = this;
-    }
+    ofxOfeliaMessageManager(int portNumber);
     
-    ~ofxOfeliaMessageManager()
-    {
-        shouldQuit = true;
-        udpThread.join();
-    }
+    ~ofxOfeliaMessageManager();
     
-    bool receiveMessage(ofxMessageType& type, std::string& message)
-    {
-        std::pair<ofxMessageType, std::string> result;
-        auto dequeued = messageQueue.try_dequeue(result);
-        if(dequeued)
-        {
-            type = result.first;
-            message = result.second;
-            return true;
-        }
-        
-        return false;
-    }
+    bool receiveMessage(ofxMessageType& type, std::string& message);
     
     // Formats message to stringstream and sends it to the other process
     template <typename... Types>
@@ -64,43 +35,7 @@ struct ofxOfeliaMessageManager {
         return ofxOfeliaStream::parseMessage<Types...>(message);
     }
     
-    void run()
-    {
-        std::string message;
-        while (!shouldQuit)
-        {
-            message = pipe.receive();
-            
-            if(message.empty())
-            {
-                std::this_thread::sleep_for(std::chrono::microseconds(500));
-                continue;
-            }
-               
-            std::string args;
-            
-            auto istream = std::stringstream(message);
-
-            // Remove type of message to return later
-            ofxMessageType messageType;
-            istream.read(reinterpret_cast<char *>(&messageType), sizeof(ofxMessageType));
-
-            /*
-            if (messageType == ofx_audio_block)
-            {
-                signalQueue.enqueue();
-                continue;
-            } */
-            
-            // Put args into string
-            char ch;
-            while (istream.get(ch)) {
-                args += ch;  // Append each character to the string
-            }
-            
-            messageQueue.enqueue({messageType, args});
-        }
-    }
+    void run();
     
     template <typename... Types>
     static std::tuple<Types...> waitForReturnValue()
