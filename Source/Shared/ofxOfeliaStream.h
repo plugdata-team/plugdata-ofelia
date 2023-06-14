62aa83a4db60004ac1362c5f28bd11dc20808a35
Timothy Schoen <44585538+timothyschoen@users.noreply.github.com>
2023-06-14 21:35:38 +0200

    Preventing symbol clashes between JUCE and Windows API

--- Source/Shared/ofxOfeliaStream.h
+++ Source/Shared/ofxOfeliaStream.h
@@ -3,28 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cstring>
-#include <cassert>
-#include <cstdint>
 #include <sstream>
-#include <vector>
-#include <tuple>
-
-#ifdef _WIN32
-#include <winsock2.h>
-#include <ws2tcpip.h>
-#pragma comment(lib, "ws2_32.lib")
-
-using socklen_t = int;
-
-#else
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <fcntl.h>
-#endif
-
-
+#include "ofxPdInterface.h"
 
 enum ofxMessageType
 {
@@ -88,67 +68,25 @@ enum ofxMessageType
     pd_get_sys_info
 };
 
+#ifdef _WIN32
+using socklen_t = int;
+#endif
 
 class ofxOfeliaStream {
 private:
     int socket_;
     int send_port_;
-    struct sockaddr_in address_;
     static constexpr size_t buffer_size = 65500;
     
 public:
-    ofxOfeliaStream() {
-#ifdef _WIN32
-        WSADATA wsaData;
-        if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-            std::cerr << "Failed to initialize Winsock." << std::endl;
-        }
-#endif
-        socket_ = socket(AF_INET, SOCK_DGRAM, 0);
-        if (socket_ < 0) {
-            std::cerr << "Failed to create socket." << std::endl;
-        }
-    }
 
-    ~ofxOfeliaStream() {
-#ifdef _WIN32
-        closesocket(socket_);
-        WSACleanup();
-#else
-        close(socket_);
-#endif
-    }
+    ofxOfeliaStream();
+
+    ~ofxOfeliaStream();
     
+    bool bind(int receive_port, int send_port);
     
-    bool bind(int receive_port, int send_port) {
-        send_port_ = send_port;
-        address_.sin_family = AF_INET;
-        address_.sin_addr.s_addr = htonl(INADDR_ANY);
-        address_.sin_port = htons(receive_port);
-
-        if (::bind(socket_, (struct sockaddr*)&address_, sizeof(address_)) < 0) {
-            std::cerr << "Failed to bind socket." << std::endl;
-            return false;
-        }
-
-        return true;
-    }
-    
-    std::string receive(bool blocking = false)
-    {
-        std::string rcvd;
-        if(blocking)
-        {
-            rcvd = receiveBlocking();
-        }
-        else
-        {
-            rcvd = receiveNonBlocking();
-        }
-        
-        return rcvd;
-    }
-
+    std::string receive(bool blocking = false);
 
     // Internal function for parsing received messages from stringstream to any number of types
     template <int I, typename Result, typename T, typename... Types>
@@ -187,7 +125,7 @@ public:
                 }
                 if(type == A_SYMBOL)
                 {
-                    #if FAKE_PD_INTERFACE
+                    #ifndef PD
                         int length;
                         istream.read(reinterpret_cast<char *>(&length), sizeof(int));
                         
@@ -234,17 +172,7 @@ public:
             parse<I + 1, Result, Types...>(istream, result);
         }
     }
-    
-    bool convertAddress(struct sockaddr_in* destAddr) {
-    #ifdef _WIN32
-        int result = InetPton(AF_INET, L"127.0.0.1", &(destAddr->sin_addr));
-        return (result == 1);
-    #else
-        int result = inet_pton(AF_INET, "127.0.0.1", &(destAddr->sin_addr));
-        return (result == 1);
-    #endif
-    }
-    
+        
     // Formats message to stringstream and sends it to the other process
     template <typename... Types>
     void sendMessage(Types... args)
@@ -252,19 +180,7 @@ public:
         std::stringstream stream;
         writeToStream(stream, args...);
 
-        struct sockaddr_in destAddr;
-        destAddr.sin_family = AF_INET;
-        destAddr.sin_port = htons(send_port_);
-        if (convertAddress(&destAddr) <= 0) {
-            std::cerr << "Failed to convert IP address." << std::endl;
-            return;
-        }
-
-        auto message = stream.str();
-        
-        ::sendto(socket_, message.c_str(), message.size(), 0,
-                        (struct sockaddr*)&destAddr, sizeof(destAddr));
-        
+        sendto(stream.str());        
     }
     
     // Function for parsing messages
@@ -348,53 +264,12 @@ private:
         }
     }
     
+    bool convertAddress(struct sockaddr_in* destAddr);
 
+    std::string receiveBlocking();
+    std::string receiveNonBlocking();
 
-    std::string receiveBlocking() {
-        char buffer[buffer_size];
-        struct sockaddr_in srcAddr;
-        socklen_t addrLen = sizeof(srcAddr);
-
-        int bytesRead = ::recvfrom(socket_, buffer, sizeof(buffer) - 1, 0,
-                                       (struct sockaddr*)&srcAddr, &addrLen);
-        if (bytesRead > 0) {
-            return std::string(buffer, bytesRead);
-        }
-
-        return {};
-    }
-
-    std::string receiveNonBlocking() {
-    #ifdef _WIN32
-        u_long mode = 1;
-        ioctlsocket(socket_, FIONBIO, &mode);
-    #else
-        int flags = fcntl(socket_, F_GETFL, 0);
-        fcntl(socket_, F_SETFL, flags | O_NONBLOCK);
-    #endif
-
-        char buffer[buffer_size];
-        struct sockaddr_in srcAddr;
-        socklen_t addrLen = sizeof(srcAddr);
-
-        int bytesRead = ::recvfrom(socket_, buffer, sizeof(buffer) - 1, 0,
-                                       (struct sockaddr*)&srcAddr, &addrLen);
-
-    #ifdef _WIN32
-        mode = 0;
-        ioctlsocket(socket_, FIONBIO, &mode);
-    #else
-        flags = fcntl(socket_, F_GETFL, 0);
-        flags &= ~O_NONBLOCK;
-        fcntl(socket_, F_SETFL, flags);
-    #endif
-
-        if (bytesRead > 0) {            
-            return std::string(buffer, bytesRead);
-        }
-
-        return {};
-    }
+    void sendto(std::string message);
     
 };
 
